da8575ee3c8f6f9f48c65bb8dad0d4135450b6a8
Angel E Moran Castillo <angelmc636belial@gmail.com>
2021-06-06 01:05:21 -0500

    footer listo header falta dspliegue de submenu

---
+++ menu.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("open").style.display = "none";
+    document.getElementById("close").style.display = "block";
+}
+function closeMenu() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("open").style.display = "block";
+    document.getElementById("close").style.display = "none";
+}
